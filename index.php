e671128e856a15a49caa38fb1745444aeedc3b1b
Waleed Halaby <Waleed Halaby>
2017-05-17 00:04:29 +0200

    Add Project To Git

---
+++ index.php
@@ -0,0 +1,303 @@
+<?php include('Shared/header.html') ?>
+
+<?php
+	$mockLatestVideosPage1 = array(
+		1 => array(
+			"Url" => "https://www.youtube.com/watch?v=6303XAWpo_o",
+			"EmbedCode" => "https://www.youtube.com/embed/6303XAWpo_o",
+			"DateTimeAdded" => "3 years ago",
+			"Description" => "Someone needs to revoke his revealance, so he can run away from his grandmother",
+			"Title" => "Someone needs to revoke his revealance",
+			"Time" => "4:12",
+			"YoutubeViews" => "154,223"
+		),
+		2 => array(
+			"Url" => "https://www.youtube.com/watch?v=oJtK2uzjFEI",
+			"EmbedCode" => "https://www.youtube.com/embed/6303XAWpo_o",
+			"DateTimeAdded" => "22 months ago",
+			"Description" => "Someone needs to revoke his revealance, so he can run away from his grandmother",
+			"Title" => "What Happened When No one was Here",
+			"Time" => "34:55",
+			"YoutubeViews" => "1,114,346"
+		),
+		3 => array(
+			"Url" => "https://www.youtube.com/watch?v=tZlq3KsMxT4",
+			"EmbedCode" => "https://www.youtube.com/embed/6303XAWpo_o",
+			"DateTimeAdded" => "3 days ago",
+			"Description" => "Someone needs to revoke his revealance, so he can run away from his grandmother",
+			"Title" => "Someone needs to revoke his revealance",
+			"Time" => "43:46",
+			"YoutubeViews" => "14,652"
+		),
+		4 => array(
+			"Url" => "https://www.youtube.com/watch?v=Opn9v1mvRaM",
+			"EmbedCode" => "https://www.youtube.com/embed/6303XAWpo_o",
+			"DateTimeAdded" => "3 months ago",
+			"Description" => "Someone needs to revoke his revealance, so he can run away from his grandmother",
+			"Title" => "Someone needs to revoke his revealance",
+			"Time" => "24:42",
+			"YoutubeViews" => "2,184,433"
+		),
+		5 => array(
+			"Url" => "https://www.youtube.com/watch?v=oJtK2uzjFEI",
+			"EmbedCode" => "https://www.youtube.com/embed/6303XAWpo_o",
+			"DateTimeAdded" => "22 months ago",
+			"Description" => "Someone needs to revoke his revealance, so he can run away from his grandmother",
+			"Title" => "What Happened When No one was Here",
+			"Time" => "34:55",
+			"YoutubeViews" => "1,114,346"
+		),
+		6 => array(
+			"Url" => "https://www.youtube.com/watch?v=Opn9v1mvRaM",
+			"EmbedCode" => "https://www.youtube.com/embed/6303XAWpo_o",
+			"DateTimeAdded" => "3 months ago",
+			"Description" => "Someone needs to revoke his revealance, so he can run away from his grandmother",
+			"Title" => "Someone needs to revoke his revealance",
+			"Time" => "24:42",
+			"YoutubeViews" => "2,184,433"
+		),
+		7 => array(
+			"Url" => "https://www.youtube.com/watch?v=6303XAWpo_o",
+			"EmbedCode" => "https://www.youtube.com/embed/6303XAWpo_o",
+			"DateTimeAdded" => "3 years ago",
+			"Description" => "Someone needs to revoke his revealance, so he can run away from his grandmother",
+			"Title" => "Someone needs to revoke his revealance",
+			"Time" => "4:12",
+			"YoutubeViews" => "154,223"
+		),
+		8 => array(
+			"Url" => "https://www.youtube.com/watch?v=oJtK2uzjFEI",
+			"EmbedCode" => "https://www.youtube.com/embed/6303XAWpo_o",
+			"DateTimeAdded" => "22 months ago",
+			"Description" => "Someone needs to revoke his revealance, so he can run away from his grandmother",
+			"Title" => "What Happened When No one was Here",
+			"Time" => "34:55",
+			"YoutubeViews" => "1,114,346"
+		),
+		9 => array(
+			"Url" => "https://www.youtube.com/watch?v=tZlq3KsMxT4",
+			"EmbedCode" => "https://www.youtube.com/embed/6303XAWpo_o",
+			"DateTimeAdded" => "3 days ago",
+			"Description" => "Someone needs to revoke his revealance, so he can run away from his grandmother",
+			"Title" => "Someone needs to revoke his revealance",
+			"Time" => "43:46",
+			"YoutubeViews" => "14,652"
+		),
+		10 => array(
+			"Url" => "https://www.youtube.com/watch?v=Opn9v1mvRaM",
+			"EmbedCode" => "https://www.youtube.com/embed/6303XAWpo_o",
+			"DateTimeAdded" => "3 months ago",
+			"Description" => "Someone needs to revoke his revealance, so he can run away from his grandmother",
+			"Title" => "Someone needs to revoke his revealance",
+			"Time" => "24:42",
+			"YoutubeViews" => "2,184,433"
+		),
+		11 => array(
+			"Url" => "https://www.youtube.com/watch?v=oJtK2uzjFEI",
+			"EmbedCode" => "https://www.youtube.com/embed/6303XAWpo_o",
+			"DateTimeAdded" => "22 months ago",
+			"Description" => "Someone needs to revoke his revealance, so he can run away from his grandmother",
+			"Title" => "What Happened When No one was Here",
+			"Time" => "34:55",
+			"YoutubeViews" => "1,114,346"
+		),
+		12 => array(
+			"Url" => "https://www.youtube.com/watch?v=Opn9v1mvRaM",
+			"EmbedCode" => "https://www.youtube.com/embed/6303XAWpo_o",
+			"DateTimeAdded" => "3 months ago",
+			"Description" => "Someone needs to revoke his revealance, so he can run away from his grandmother",
+			"Title" => "Someone needs to revoke his revealance",
+			"Time" => "24:42",
+			"YoutubeViews" => "2,184,433"
+		)
+	);
+
+	$mockLatestVideosPage2 = array(
+		13 => array(
+			"Url" => "https://www.youtube.com/watch?v=6303XAWpo_o",
+			"EmbedCode" => "https://www.youtube.com/embed/6303XAWpo_o",
+			"DateTimeAdded" => "3 years ago",
+			"Description" => "Someone needs to revoke his revealance, so he can run away from his grandmother",
+			"Title" => "Someone needs to revoke his revealance",
+			"Time" => "4:12",
+			"YoutubeViews" => "154,223"
+		),
+		14 => array(
+			"Url" => "https://www.youtube.com/watch?v=oJtK2uzjFEI",
+			"EmbedCode" => "https://www.youtube.com/embed/6303XAWpo_o",
+			"DateTimeAdded" => "22 months ago",
+			"Description" => "Someone needs to revoke his revealance, so he can run away from his grandmother",
+			"Title" => "What Happened When No one was Here",
+			"Time" => "34:55",
+			"YoutubeViews" => "1,114,346"
+		),
+		15 => array(
+			"Url" => "https://www.youtube.com/watch?v=tZlq3KsMxT4",
+			"EmbedCode" => "https://www.youtube.com/embed/6303XAWpo_o",
+			"DateTimeAdded" => "3 days ago",
+			"Description" => "Someone needs to revoke his revealance, so he can run away from his grandmother",
+			"Title" => "Someone needs to revoke his revealance",
+			"Time" => "43:46",
+			"YoutubeViews" => "14,652"
+		),
+		16 => array(
+			"Url" => "https://www.youtube.com/watch?v=Opn9v1mvRaM",
+			"EmbedCode" => "https://www.youtube.com/embed/6303XAWpo_o",
+			"DateTimeAdded" => "3 months ago",
+			"Description" => "Someone needs to revoke his revealance, so he can run away from his grandmother",
+			"Title" => "Someone needs to revoke his revealance",
+			"Time" => "24:42",
+			"YoutubeViews" => "2,184,433"
+		),
+		17 => array(
+			"Url" => "https://www.youtube.com/watch?v=oJtK2uzjFEI",
+			"EmbedCode" => "https://www.youtube.com/embed/6303XAWpo_o",
+			"DateTimeAdded" => "22 months ago",
+			"Description" => "Someone needs to revoke his revealance, so he can run away from his grandmother",
+			"Title" => "What Happened When No one was Here",
+			"Time" => "34:55",
+			"YoutubeViews" => "1,114,346"
+		),
+		18 => array(
+			"Url" => "https://www.youtube.com/watch?v=Opn9v1mvRaM",
+			"EmbedCode" => "https://www.youtube.com/embed/6303XAWpo_o",
+			"DateTimeAdded" => "3 months ago",
+			"Description" => "Someone needs to revoke his revealance, so he can run away from his grandmother",
+			"Title" => "Someone needs to revoke his revealance",
+			"Time" => "24:42",
+			"YoutubeViews" => "2,184,433"
+		),
+		19 => array(
+			"Url" => "https://www.youtube.com/watch?v=6303XAWpo_o",
+			"EmbedCode" => "https://www.youtube.com/embed/6303XAWpo_o",
+			"DateTimeAdded" => "3 years ago",
+			"Description" => "Someone needs to revoke his revealance, so he can run away from his grandmother",
+			"Title" => "Someone needs to revoke his revealance",
+			"Time" => "4:12",
+			"YoutubeViews" => "154,223"
+		),
+		20 => array(
+			"Url" => "https://www.youtube.com/watch?v=oJtK2uzjFEI",
+			"EmbedCode" => "https://www.youtube.com/embed/6303XAWpo_o",
+			"DateTimeAdded" => "22 months ago",
+			"Description" => "Someone needs to revoke his revealance, so he can run away from his grandmother",
+			"Title" => "What Happened When No one was Here",
+			"Time" => "34:55",
+			"YoutubeViews" => "1,114,346"
+		),
+		21 => array(
+			"Url" => "https://www.youtube.com/watch?v=tZlq3KsMxT4",
+			"EmbedCode" => "https://www.youtube.com/embed/6303XAWpo_o",
+			"DateTimeAdded" => "3 days ago",
+			"Description" => "Someone needs to revoke his revealance, so he can run away from his grandmother",
+			"Title" => "Someone needs to revoke his revealance",
+			"Time" => "43:46",
+			"YoutubeViews" => "14,652"
+		),
+		22 => array(
+			"Url" => "https://www.youtube.com/watch?v=Opn9v1mvRaM",
+			"EmbedCode" => "https://www.youtube.com/embed/6303XAWpo_o",
+			"DateTimeAdded" => "3 months ago",
+			"Description" => "Someone needs to revoke his revealance, so he can run away from his grandmother",
+			"Title" => "Someone needs to revoke his revealance",
+			"Time" => "24:42",
+			"YoutubeViews" => "2,184,433"
+		),
+		23 => array(
+			"Url" => "https://www.youtube.com/watch?v=oJtK2uzjFEI",
+			"EmbedCode" => "https://www.youtube.com/embed/6303XAWpo_o",
+			"DateTimeAdded" => "22 months ago",
+			"Description" => "Someone needs to revoke his revealance, so he can run away from his grandmother",
+			"Title" => "What Happened When No one was Here",
+			"Time" => "34:55",
+			"YoutubeViews" => "1,114,346"
+		)
+	);
+
+	$mockLatestVideos = $mockLatestVideosPage1;
+
+	$totalRows = 24;
+	$numOfPages = ceil($totalRows / 10);
+
+	if(!empty($_GET['page'])){
+		if($_GET['page'] == 1){
+			$mockLatestVideos = $mockLatestVideosPage1;
+		}
+		elseif($_GET['page'] == 2){
+			$mockLatestVideos = $mockLatestVideosPage2;
+		}
+	}
+	if(!empty($_GET['page'])){
+		if($_GET['page'] == 1)
+			$html = "<div class='text-center'>
+					<ul class='pagination'>
+						<li class='disabled'><a href=''>&laquo;</a></li>";
+		elseif($_GET['page'] == 2)
+			$html = "<div class='text-center'>
+					<ul class='pagination'>
+						<li><a href='?page=1'>&laquo;</a></li>";
+	}
+	else
+		$html = "<div class='text-center'>
+					<ul class='pagination'>
+						<li class='disabled'><a href=''>&laquo;</a></li>";
+
+	for($i = 1 ; $i < $numOfPages ; $i ++){
+		$html =$html . "<li><a href='?page=".$i."'>".$i."</a></li>";
+	}
+	
+	if(!empty($_GET['page'])){
+		if($_GET['page'] == 1)
+			$html = $html ."<li><a href='?page=".($numOfPages - 1)."'>&raquo;</a></li></ul></div>";
+		elseif($_GET['page'] == 2)
+			$html = $html ."<li class='disabled'><a href=''>&raquo;</a></li></ul></div>";
+	}
+	else
+		$html = $html ."<li><a href='?page=".($numOfPages - 1)."'>&raquo;</a></li></ul></div>";
+ ?>
+
+<ul class="nav nav-tabs">
+  <li class="active"><a href="#latest" data-toggle="tab" aria-expanded="true">Latest</a></li>
+  <li class=""><a href="#trending" data-toggle="tab" aria-expanded="false">Trending</a></li>
+  <li class=""><a href="#mostViewed" data-toggle="tab" aria-expanded="false">Most Viewed</a></li>
+</ul>
+<div id="myTabContent" class="tab-content">
+  <div class="tab-pane fade active in" id="latest">
+    <?php 
+    	echo "<div class='row'>";
+    	foreach ($mockLatestVideos as $key => $video) {
+    		echo "
+    			<a href='video.php?id=".$key."' data-toggle='tooltip' data-replacement='top'>
+  					<div class='col-sm-6 col-md-3'>
+					    <div class='thumbnail'>
+					      <img class='img-thumbnail' src='http://img.youtube.com/vi/".str_replace('https://www.youtube.com/watch?v=','',$video['Url'])."/0.jpg' alt='...'>
+					      <label class='label label-info pull-right'>".$video['Time']."</label>
+					      <div class='caption'>
+					        <h4>".$video['Title']."</h4>
+					       	<label>".$video['DateTimeAdded']."</label>
+					       	<label class='pull-right'>".$video['YoutubeViews']." Views</label>
+					      </div>
+					    </div>
+			  		</div>
+			    </a>
+			    <div class='tooltip tooltip-top' role='tooltip'>
+				  <div class='tooltip-arrow'></div>
+				  <div class='tooltip-inner'>
+				    ".$video['Description']."
+				  </div>
+				</div>";
+    	}
+    	echo "</div>";
+
+    	echo $html;
+    ?>
+  </div>
+  <div class="tab-pane fade" id="trending">
+    <p>Food truck fixie locavore, accusamus mcsweeney's marfa nulla single-origin coffee squid. Exercitation +1 labore velit, blog sartorial PBR leggings next level wes anderson artisan four loko farm-to-table craft beer twee. Qui photo booth letterpress, commodo enim craft beer mlkshk aliquip jean shorts ullamco ad vinyl cillum PBR. Homo nostrud organic, assumenda labore aesthetic magna delectus mollit.</p>
+  </div>
+  <div class="tab-pane fade" id="mostViewed">
+    <p>Food truck fixie locavore, accusamus mcsweeney's marfa nulla single-origin coffee squid. Exercitation +1 labore velit, blog sartorial PBR leggings next level wes anderson artisan four loko farm-to-table craft beer twee. Qui photo booth letterpress, commodo enim craft beer mlkshk aliquip jean shorts ullamco ad vinyl cillum PBR. Homo nostrud organic, assumenda labore aesthetic magna delectus mollit.</p>
+  </div>
+</div>
+
+<?php include('Shared/footer.html') ?>
